5dcae422dd571967b93644ae76fb66b1305aef75
TLNieves <tnieves@correo.um.edu.uy>
2022-08-30 18:57:10 -0300

    aa

--- src/main/java/com/example/restaurante/HelloApplication.java
+++ src/main/java/com/example/restaurante/HelloApplication.java
@@ -11,7 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import 
+        javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
